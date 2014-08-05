d1f626c39ab2ad4f5acc17ffead820cc1f17a41b
unknown <mkirchmann@PD-WG000404.prodyna.com>
2014-08-05 18:35:30 +0200

    now allow stupid answers

--- de.neuenberger.pmp.processes/src/main/java/de/neuenberger/pmp/processes/generator/KnowledgeAreaReleatedAdditionalQuestionsGenerator.java
+++ de.neuenberger.pmp.processes/src/main/java/de/neuenberger/pmp/processes/generator/KnowledgeAreaReleatedAdditionalQuestionsGenerator.java
@@ -8,50 +8,66 @@ import java.util.LinkedList;
 import java.util.List;
 
 import de.neuenberger.pmp.processes.generator.QuestionUtil.LabelProvider;
-import de.neuenberger.pmp.processes.model.Question;
 import de.neuenberger.pmp.processes.model.DefaultQuestionGroup;
+import de.neuenberger.pmp.processes.model.Question;
 
-public class KnowledgeAreaReleatedAdditionalQuestionsGenerator extends AbstractLazyQuestionContainer{
+public class KnowledgeAreaReleatedAdditionalQuestionsGenerator extends
+		AbstractLazyQuestionContainer {
 	private final CplxKnowledgeArea knowledgeArea;
-	private DefaultQuestionGroup questionGroup;
-	public KnowledgeAreaReleatedAdditionalQuestionsGenerator(CplxKnowledgeArea knowledgeArea) {
-		super("Definitions for "+knowledgeArea.getName());
+	private final DefaultQuestionGroup questionGroup;
+
+	public KnowledgeAreaReleatedAdditionalQuestionsGenerator(
+			final CplxKnowledgeArea knowledgeArea) {
+		super("Definitions for " + knowledgeArea.getName());
 		this.knowledgeArea = knowledgeArea;
 		questionGroup = new DefaultQuestionGroup(getName());
 	}
 
-	
 	@Override
 	protected List<Question> createQuestions() {
-		List<Question> questions = new LinkedList<>();
-		if (knowledgeArea.getAddition()!=null) {
-			List<CplxGroup> groups = knowledgeArea.getAddition().getGroup();
-			for (CplxGroup cplxGroup : groups) {
+		final List<Question> questions = new LinkedList<>();
+		if (knowledgeArea.getAddition() != null) {
+			final List<CplxGroup> groups = knowledgeArea.getAddition()
+					.getGroup();
+			for (final CplxGroup cplxGroup : groups) {
 				questions.addAll(createQuestionForProcess(cplxGroup));
 			}
 		}
 		return questions;
 	}
-	
-	public List<Question> createQuestionForProcess(CplxGroup drawnProcess) {
-		List<CplxDefinition> definition = drawnProcess.getDefinition();
-		DefinitionDefinitionLabelProvider provider = new DefinitionDefinitionLabelProvider();
-		
-		List<Question> result = new LinkedList<>();
-		for (CplxDefinition cplxDefinition : definition) {
-			result.add(QuestionUtil.createQuestion(questionGroup, "Looking at "+drawnProcess.getName()+", what is defined by '"+cplxDefinition.getDescription()+"'?", cplxDefinition, definition));
-			result.add(QuestionUtil.createQuestion(questionGroup, "Looking at "+drawnProcess.getName()+", what is defining '"+cplxDefinition.getName()+"'?", cplxDefinition, definition, provider));
+
+	public List<Question> createQuestionForProcess(final CplxGroup drawnProcess) {
+		final List<CplxDefinition> definition = drawnProcess.getDefinition();
+		final DefinitionDefinitionLabelProvider provider = new DefinitionDefinitionLabelProvider();
+
+		final List<Question> result = new LinkedList<>();
+		for (final CplxDefinition cplxDefinition : definition) {
+			if (!Boolean.TRUE.equals(cplxDefinition.isIgnoreAsQuestion())) {
+				result.add(QuestionUtil.createQuestion(
+						questionGroup,
+						"Looking at " + drawnProcess.getName()
+								+ ", what is defined by '"
+								+ cplxDefinition.getDescription() + "'?",
+						cplxDefinition, definition));
+				result.add(QuestionUtil.createQuestion(
+						questionGroup,
+						"Looking at " + drawnProcess.getName()
+								+ ", what is defining '"
+								+ cplxDefinition.getName() + "'?",
+						cplxDefinition, definition, provider));
+			}
 		}
-		
+
 		return result;
 	}
-	
-	public static class DefinitionDefinitionLabelProvider implements LabelProvider<CplxDefinition> {
+
+	public static class DefinitionDefinitionLabelProvider implements
+			LabelProvider<CplxDefinition> {
 
 		@Override
-		public String getLabel(CplxDefinition e) {
+		public String getLabel(final CplxDefinition e) {
 			return e.getDescription();
 		}
-		
+
 	}
 }
